f69ebb7ae06f452b40101afa80ea1ff73c645f4c
crater2150 <me@crater2150.de>
2012-01-13 19:22:36 +0100

    pidfile locking

--- src/tempgov.c
+++ src/tempgov.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <string.h>
 #include <stdio.h>
 #include <glob.h>
@@ -6,6 +8,10 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/file.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 #include <syslog.h>
 
 #define SYS_GLOB_CPUS  "/sys/devices/system/cpu/cpu?/cpufreq/scaling_governor"
@@ -17,17 +23,43 @@
 #define SYSLOG_IDENT   "tempgov"
 #define SYSLOG_FORMAT  "Switching to governor \"%s\""
 
+#define PIDFILE_PATH   "/var/run/tempgov.pid"
+
+#define MSG_RUNNING    "Another tempgov instance is running already\n"
+#define MSG_STARTING   "Starting temperature supervising"
+
+#define FMT_MSG_ADD_CPU "Adding CPU: %s"
+
 #define CHECK_FREQUENCY  8
 #define TEMP_THRESHOLD   92000
 static void setgov(const char* governor);
 static void cleanup(int signal);
 static char* tempgov();
 FILE** govhandles;
+int pidfile_fd;
+FILE* pidfile;
 
 int main(int argc, char* argv[])
 {
 
 	pid_t pid, sid;
+
+	pidfile_fd = open(PIDFILE_PATH, O_WRONLY | O_CREAT);
+	int lock_success = flock(pidfile_fd, LOCK_EX | LOCK_NB);
+
+	if(lock_success == -1) {
+		if(errno == EWOULDBLOCK) {
+			printf(MSG_RUNNING);
+		} else {
+			perror("PID file locking failed");
+		}
+		exit(EXIT_FAILURE);
+	}
+
+	ftruncate(pidfile_fd, 0);
+
+	pidfile = fdopen(pidfile_fd, "w");
+
 
 	pid = fork();
 	if (pid < 0) {
@@ -47,6 +79,9 @@ int main(int argc, char* argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	pid_t ownpid = getpid();
+	fprintf(pidfile, "%d\n", (int) ownpid);
+
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
@@ -63,14 +98,14 @@ int main(int argc, char* argv[])
 	govhandles = malloc(sizeof(FILE*) * pglob.gl_pathc + 1);
 	for(int i = 0; i < pglob.gl_pathc; i++) {
 		govhandles[i] = fopen(pglob.gl_pathv[i],"w");
-		syslog(LOG_INFO, "Adding CPU: %s", pglob.gl_pathv[i]);
+		syslog(LOG_INFO, FMT_MSG_ADD_CPU, pglob.gl_pathv[i]);
 	}
 	govhandles[pglob.gl_pathc] = NULL;
 	globfree(&pglob);
 
 	signal(15, &cleanup);
 
-	syslog(LOG_INFO, "Starting temperature supervising");
+	syslog(LOG_INFO,MSG_STARTING);
 	while(1) {
 		sleep(8);
 		char* gov = tempgov();
@@ -88,6 +123,8 @@ void cleanup(int signal)
 	for(FILE** i = govhandles;*i != NULL; i++) {
 		fclose(*i);
 	}
+	flock(pidfile_fd, LOCK_UN);
+	fclose(pidfile);
 
 }
 static void setgov(const char* governor)
